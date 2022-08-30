76c9a1529212aee68736894add4712f824bb1aa3
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-08-30 13:21:11 +0530

    login user updated

--- src/pages/session/Login/user/index.jsx
+++ src/pages/session/Login/user/index.jsx
@@ -1,8 +1,14 @@
-import { Grid, Typography } from "@mui/material";
-import React, { Component } from "react";
+import {Grid, Typography} from "@mui/material";
+import React, {Component} from "react";
+import {TextValidator, ValidatorForm} from "react-material-ui-form-validator";
+import {styleSheet} from "./style";
+import {withStyles} from "@mui/styles";
 import CommonButton from "../../../../components/common/Button";
-import { TextValidator, ValidatorForm } from "react-material-ui-form-validator";
-import {Link} from "react-router-dom";
+import localStorageService from "../../../service/localStorageService";
+import LoginService from "../../../service/LoginService";
+import jwt_decode from "jwt-decode";
+import CustomSnackBar from "../../../../components/common/SnackBar";
+import {withRouter} from "../../../withRouter";
 
 class LoginUser extends Component {
     constructor(props) {
@@ -12,21 +18,46 @@ class LoginUser extends Component {
                 userName: "",
                 password: "",
             },
+            alert: false,
+            message: '',
+            severity: '',
         };
     }
 
     handleSubmit = async () => {
         console.log("Hi handle");
         console.log(this.state.formData);
+
+        let fData = this.state.formData
+        let res = await LoginService.postLogin(fData);
+        if (res.status === 200) {
+            let token = res.data.token;
+            let decoded = jwt_decode(token);
+            localStorageService.setItem("user", decoded.user)
+            // console.log(decoded)
+            // window.location.replace("http://www.w3schools.com");
+            this.props.navigate('/dashboard')
+            // window.location.reload();
+        } else {
+
+            this.setState({
+                alert: true, message: 'Please Check the Email & Password!', severity: 'error'
+            });
+
+
+        }
+
+        // localStorage.setItem("user",this.state.username)
+
     };
 
     handleChange = (event) => {
         let id = event.target.name;
         switch (id) {
-            case "userName":
-                const userName = event.target.value;
+            case "username":
+                const username = event.target.value;
                 this.setState(
-                    Object.assign(this.state.formData, { userName: userName })
+                    Object.assign(this.state.formData, {username: username})
                 );
                 // this.setState({ userId });
                 break;
@@ -34,7 +65,7 @@ class LoginUser extends Component {
             case "password":
                 const password = event.target.value;
                 this.setState(
-                    Object.assign(this.state.formData, { password: password })
+                    Object.assign(this.state.formData, {password: password})
                 );
                 break;
 
@@ -42,46 +73,55 @@ class LoginUser extends Component {
                 break;
         }
     };
-
     render() {
+        // if (this.state.redirect){
+        //     <Navigate to="/dashboard" replace={true}/>
+        // }
+        const {classes} = this.props;
         return (
-            <Grid
-                container
-                direction={"column"}
-                alignItems="center"
-                justifyContent={"center"}
-                // className="h-screen w-screen bg-red-200"
-                className='pt-28'
-            >
-                <Grid container direction={"column"} alignItems="center">
-                    <Grid
-                        item
-                        container
-                        className="min-h-96  w-96 bg-slate-50 rounded-lg p-10 "
-                    >
-                        <Grid container item direction={"column"} gap="20px" className="">
-                            {/* <Grid item>
-                <Typography
-                  variant="h4"
-                  className="text-center uppercase font-bold"
+            <>
+                <Grid
+                    container
+                    direction={"row"}
+                    justifyContent="center"
+                    alignItems="stretch"
+                    className="h-screen min-h-min w-screen p-0 m-0 bg-login-img bg-no-repeat bg-bottoms bg-cover bg-center bg-blend-overlay bg-stone-700"
                 >
-                  Login
-                </Typography>
-              </Grid> */}
+                    <Grid
+                        container
+                        direction={"row"}
+                        item
+                        xs={12}
+                        md={12}
+                        lg={12}
+                        justifyContent="center"
+                        alignItems="center"
+                    >
+                        <div
+                            className="flex gap-6 flex-col h-fit w-96 rounded-xl bg-blue-300 p-10 m-5 md:m-0 bg-opacity-10  backdrop-blur-sm"
+                            style={{border: "1px solid rgba(255, 255, 255, 0.09)"}}
+                        >
+                            <Grid item>
+                                <Typography
+                                    variant="h4"
+                                    className="text-center uppercase font-bold text-white"
+                                >
+                                    Login
+                                </Typography>
+                            </Grid>
                             <ValidatorForm
                                 onSubmit={this.handleSubmit}
                                 onError={(errors) => console.log(errors)}
                             >
-                                <Grid item container direction={"column"} alignItems rowGap="20px">
+                                <Grid item container direction={"column"} rowGap="20px">
                                     <TextValidator
                                         label="User Name"
                                         onChange={this.handleChange}
-                                        name="userName"
-                                        value={this.state.formData.userName}
+                                        name="username"
+                                        value={this.state.formData.username}
                                         validators={["required"]}
                                         errorMessages={["This field is required"]}
-                                        className="w-full"
-                                        style={{minWidth:'100%'}}
+                                        className={[classes.textField, "w-full text-red-500"]}
                                     />
                                     <TextValidator
                                         label="Password"
@@ -91,30 +131,43 @@ class LoginUser extends Component {
                                         validators={["required"]}
                                         errorMessages={["This field is required"]}
                                         type={"password"}
-                                        className="w-full"
-                                        style={{minWidth:'100%'}}
+                                        className={[classes.textField, "w-full"]}
                                     />
-                                    <Typography variant="p" className="text-slate-500 -mt-3">
-                                        Forgot password ?
-                                    </Typography>
+                                    {/*<Link to="dashboard">*/}
                                     <CommonButton
                                         size="large"
                                         variant="contained"
                                         label="Login"
                                         type="submit"
-                                        className="text-white bg-blue-500 font-bold tracking-wide "
-                                        style={{backgroundColor: 'rgba(25, 118, 210, 0.95)', width: '20%'}}
+                                        onClick={() => {
+                                            this.checkValidity();
+                                        }}
+                                        className="text-white w-full bg-blue-500 font-bold tracking-wide"
                                     />
+                                    {/*</Link>*/}
 
-                                    <Typography variant="p">
-                                        You are not a member? <Link to={'/register'}> Register Now</Link>
+                                    <Typography variant="p" className={'text-white'}>
+                                        You are not a member? <Link to={'/register'}
+                                                                    className={'font-semibold'}> Register
+                                        Now</Link>
                                     </Typography>
+
                                 </Grid>
                             </ValidatorForm>
-                        </Grid>
+                        </div>
                     </Grid>
                 </Grid>
-            </Grid>
+                <CustomSnackBar
+                    open={this.state.alert}
+                    onClose={() => {
+                        this.setState({alert: false})
+                    }}
+                    message={this.state.message}
+                    autoHideDuration={3000}
+                    severity={this.state.severity}
+                    variant={'filled'}
+                />
+            </>
         );
     }
 }
