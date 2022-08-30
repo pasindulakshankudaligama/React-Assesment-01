ca68c6d857e8303012d105c30e029a5062cdda3e
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-08-30 13:25:04 +0530

    login service updated

--- src/services/LoginService.js
+++ src/services/LoginService.js
@@ -2,6 +2,16 @@ import axios from "../axios";
 import qs from 'qs'
 
 class LoginService{
+     postLogin=async (data)=>{
+        const promise = new Promise((resolve, reject) => {
+            axios.post('auth/login',data).then((res)=>{
+                return resolve(res)
+            }).catch((err)=>{
+                return resolve(err)
+            })
+        })
+        return await promise;
+    }
 
 }
 export default new LoginService();
